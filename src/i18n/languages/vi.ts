83e23e18f4c7cf901875a998ea1c5bb04960b29d
Nguyen LAM <lamtercqh@gmail.com>
2025-01-02 22:24:05 +0700

    hehe

---
+++ src/i18n/languages/vi.ts
@@ -0,0 +1,38 @@
+import Key from '../i18nKey'
+import type { Translation } from '../translation'
+
+export const vi: Translation = {
+  [Key.home]: 'Trang chủ',
+  [Key.about]: 'Thông tin',
+  [Key.archive]: 'Lưu trữ',
+  [Key.search]: 'Tìm kiếm',
+
+  [Key.tags]: 'Nhãn',
+  [Key.categories]: 'Loại',
+  [Key.recentPosts]: 'Bài viết gần đây',
+
+  [Key.comments]: 'Bình luận',
+
+  [Key.untitled]: 'Không có tiêu đề',
+  [Key.uncategorized]: 'Chưa được phân loại',
+  [Key.noTags]: 'Không có nhãn',
+
+  [Key.wordCount]: 'từ',
+  [Key.wordsCount]: 'từ',
+  [Key.minuteCount]: 'phút',
+  [Key.minutesCount]: 'phút',
+  [Key.postCount]: 'bài viết',
+  [Key.postsCount]: 'bài viết',
+
+  [Key.themeColor]: 'Màu',
+
+  [Key.lightMode]: 'Sáng',
+  [Key.darkMode]: 'Tối',
+  [Key.systemMode]: 'Hệ thống',
+
+  [Key.more]: 'Thêm',
+
+  [Key.author]: 'Tác giả',
+  [Key.publishedAt]: 'Đăng lúc',
+  [Key.license]: 'Bản quyền',
+}
